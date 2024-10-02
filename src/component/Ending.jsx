4e87f6dffd92cb3d6aa1a81295b97fafbfaa1fe7
Yogesh 2003 <x10yogesh@gmail.com>
2024-10-02 13:04:34 +0530

    change name

--- src/component/Ending.jsx
+++ src/component/Ending.jsx
@@ -2,7 +2,7 @@ export default function Ending(){
     return(
         <footer className="mt-32 text-[#8750f7]">
             <div className="h-24 bg-black flex justify-center p-9">
-           <p>© 2024 Designed and developed by yogesh kumar. All Rights Reserved.
+           <p>© 2024 Designed and developed by Yogesh Kumar. All Rights Reserved.
            </p>
            </div>
         </footer>
